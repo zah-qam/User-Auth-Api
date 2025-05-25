6f8b09e4864eb0dd11575fcc2de56fa6b60ea0e3
zah-qam <zahqam.97@outlook.com>
2025-05-25 17:50:24 +0200

    Added AppDbContext.cs whith code inside, in Context the map.

---
+++ UserAuthApi/DbContext/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAuthApi.Models;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username)
+            .IsUnique();
+    }
+}

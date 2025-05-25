67d1666569660527ea4936cdf995d0f819852b58
zah-qam <zahqam.97@outlook.com>
2025-05-25 17:53:07 +0200

    Added DbContext in Program.cs

--- UserAuthApi/Program.cs
+++ UserAuthApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace UserAuthApi
 {
     public class Program
@@ -10,6 +12,8 @@ namespace UserAuthApi
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();

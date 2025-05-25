c348365ebb01f7228c2a44b60953fb64c2a53bdb
zah-qam <zahqam.97@outlook.com>
2025-05-25 17:40:45 +0200

    Installed necessary NuGet Packages. Created User.cs with code inside it.

---
+++ UserAuthApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAuthApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
